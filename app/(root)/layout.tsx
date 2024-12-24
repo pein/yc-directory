67c35bfaf92b06fecf66eb05e1a3e9a43c5b44a4
Ilyes Houha <mi.houha@gmail.com>
2024-12-24 18:30:06 +0100

    github config and nvbar component

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
+};
