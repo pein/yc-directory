961a10b94f935f9f8e9dbf023abe99432ad9bc4d
Ilyes Houha <mi.houha@gmail.com>
2024-12-24 21:41:32 +0100

    navbar options to show login logout depending on user status

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,13 @@
-import Navbar from "../components/Navbar";
+import Navbar from "@/app/components/Navbar";
 
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <main className="font-work-sans">
       <Navbar />
+
       {children}
     </main>
   );
 };
+
+export default layout;
